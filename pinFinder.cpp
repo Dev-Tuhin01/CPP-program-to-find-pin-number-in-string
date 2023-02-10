07ddfc7f66188580bee121f18c2d1e5fa72baa5f
Tuhin <dev.tuhin.1@gmail.com>
2023-02-10 19:46:37 +0530

    test string added

--- pinFinder.cpp
+++ pinFinder.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Sring Finder" << endl;
+    //* The string "address" is the test string
+    //* it will be searched for the 6 digit address pin
+    //* if all things run correct, the desired output will be 733134
+    string address = "Tuhin Shubhra Chakraborty, 22/3/b Milanpara, Raiganj, U/D, 733134,jsojaiuoriuoeu"; 
+
+    //?debug line
+    cout << address << endl; //!disposable code
+
+    
+
+
     return 0;
 }
